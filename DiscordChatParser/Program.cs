fee5ffff49aaaa3c903df8282d095db7d21e7205
mattmazzone <73369339+mattmazzone@users.noreply.github.com>
2022-08-09 22:55:32 -0400

    quick proof of concept

--- DiscordChatParser/Program.cs
+++ DiscordChatParser/Program.cs
@@ -1,2 +1,77 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DiscordChatParser {
+    class Program
+    {
+        static void Main()
+        {
+
+
+            using StreamReader r = new(@"C:\Users\matte\source\repos\DiscordChatParser\DiscordChatParser\MauxyLogsChannel.json");
+            // Read and parse json file
+            string json = r.ReadToEnd();
+            JObject jsonObj = JObject.Parse(json);
+
+            JToken? jsonToken = jsonObj["messages"];
+            if (jsonToken is null)
+            {
+                Console.WriteLine("Json token is null");
+                return;
+            }
+
+            // Convert to string
+            string results = jsonToken.ToString();
+
+            var messageList = JsonConvert.DeserializeObject<List<Messages>>(results);
+
+            foreach (Messages message in messageList)
+            {
+
+
+                
+    
+            if (message.Embeds.Count > 0)
+                {
+                    string name = Utils.GetDiscordNameFromDescription(message.Embeds[0].Description);
+                    string action = Utils.GetActionFromDescription(message.Embeds[0].Description);
+                    string channel = Utils.GetTargetChannel(message.Embeds[0].Description);
+
+                Console.WriteLine(name + " " + action + " " + channel + " at " + message.Timestamp);
+                }
+
+            }
+        }
+
+
+
+    }
+
+    class Messages {
+        public string? Id { get; set; }
+        public string? Type { get; set; }
+        public string? Timestamp { get; set; }
+        public string? TimestampEdited { get; set; }
+        public string? CallEndedTimestamp { get; set; }
+        public string? IsPinned { get; set; }
+        public string? Content { get; set; }
+
+        // Author
+
+        // public string? Attachments { get; set; }
+
+        public List<EmbedsMessage>? Embeds { get; set; }
+
+        // public string? Stickers { get; set; }
+        // public string? Reactions { get; set; }
+        // public string? Mentions { get; set; }
+
+
+        public class EmbedsMessage
+        {
+            public string? Description { get; set; }
+        }
+
+
+    }
+}
---
+++ DiscordChatParser/Utils.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordChatParser
+{
+    public class Utils
+    {
+        public static string GetDiscordNameFromDescription(string description)
+        {
+
+            if (description.Length < 2)
+            {
+                return description;
+            }
+
+            description = description.Substring(2);
+
+            if (description.IndexOf('*') - 1 == -1)
+            {
+                return description;
+            }
+
+            if (description.IndexOf('*') == -1)
+            {
+                return "";
+            }
+
+
+            return description.Substring(0, description.IndexOf('*') - 1);            
+        }
+
+
+        public static string GetActionFromDescription(string description)
+        {
+            if (description.Contains("joined"))
+            {
+                return "joined";
+            }
+            else if (description.Contains("left"))
+            {
+                return "left";
+            }
+            else if (description.Contains("moved"))
+            {
+                return "joined";
+            }
+            else
+            {
+                return "unknown action";
+            }
+        }
+
+        public static string GetTargetChannel(string description)
+        {
+
+            // Treat moving channel case
+
+
+            if (description.Contains("voice channel"))
+            {
+                var channel = description.Split(':', StringSplitOptions.None);
+                channel[1] = channel[1].Trim();
+                return channel[1].TrimEnd('.');
+            }
+            else
+            {
+                return description;
+            }
+        }
+
+
+
+    }
+}
